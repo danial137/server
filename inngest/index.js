8886fdcea51e21a2586631443b938bd1a793e202
danial137 <danial79fakhrabadi@gmail.com>
2025-09-13 17:48:06 +0330

    fix inngest

--- inngest/index.js
+++ inngest/index.js
@@ -11,15 +11,17 @@ const syncUserCreation = inngest.createFunction(
     { event: 'clerk/user.created' },
 
     async ({ event }) => {
-        const { id, first_name, last_name, email_address, image_url } = event.data
+        const { id, first_name, last_name, email_addresses, image_url } = event.data;
+
         const userData = {
             _id: id,
-            email: email_address[0].email_address,
-            name: first_name + '' + last_name,
+            email: email_addresses[0]?.email_address || "", // safe access
+            name: `${first_name || ""} ${last_name || ""}`,
             image: image_url
-        }
+        };
 
-        await User.create(userData)
+        await User.create(userData);
+
 
     }
 
