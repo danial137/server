f55948ea3cc700433f479163a78d2254cfbf7c68
danial137 <danial79fakhrabadi@gmail.com>
2025-09-17 15:11:19 +0330

    up to date

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ import { clerkClient } from '@clerk/express';
 
 export const protectAdmin = async (req, res, next) => {
     try {
-        const { userId } = req.auth(); // توجه: req.auth() به عنوان فانکشن
+        const { userId } = req.auth(); 
         const user = await clerkClient.users.getUser(userId);
 
         if (!user.privateMetadata.role || user.privateMetadata.role !== 'admin') {
